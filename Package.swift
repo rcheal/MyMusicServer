4529afba81c2d294c042ac23bd6e0ce09581abf0
rcheal <52130968+rcheal@users.noreply.github.com>
2022-05-09 17:38:39 -0400

    Change db from SQLite to Postgres

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.50.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.4.0"),
-        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.1.0"),
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
         .package(name: "MusicMetadata", url: "https://github.com/rcheal/MusicMetadata.git", from: "1.0.0")
     ],
     targets: [
@@ -18,7 +18,7 @@ let package = Package(
             name: "App",
             dependencies: [
                 .product(name: "Fluent", package: "fluent"),
-                .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
+                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
                 .product(name: "Vapor", package: "vapor"),
                 .byName(name: "MusicMetadata")
             ],
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,7 +1,7 @@
 import Vapor
 import Fluent
 import FluentSQL
-import FluentSQLiteDriver
+import FluentPostgresDriver
 
 // configures your application
 public func configure(_ app: Application) throws {
@@ -38,12 +38,17 @@ public func configure(_ app: Application) throws {
         try? fm.createDirectory(at: baseURL, withIntermediateDirectories: true)
     }
     Datastore.baseURL = baseURL
-    if isMemory {
-        app.databases.use(.sqlite(.memory), as: .sqlite)
-    } else {
-        app.databases.use(.sqlite(.file("MyMusicPi.sqlite")), as: .sqlite)
+    switch app.environment {
+    case .production:
+        app.databases.use(.postgres(hostname: "localhost", port: 5432, username: "bob", password: "", database: "mymusic"), as: .psql)
+    case .testing:
+        app.databases.use(.postgres(hostname: "localhost", port: 5432, username: "bob", password: "", database: "mymusictest"), as: .psql)
+    case .development:
+        app.databases.use(.postgres(hostname: "localhost", port: 5432, username: "bob", password: "", database: "mymusicdev"), as: .psql)
+    default:
+        break
     }
-    
+
     Datastore.db = app.db
     let _ = Datastore.create(memory: isMemory)
 
