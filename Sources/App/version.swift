0680a8685b7d347cf51213580dee560a02959830
rcheal <52130968+rcheal@users.noreply.github.com>
2024-06-05 16:59:57 -0400

    Update version

--- Sources/App/version.swift
+++ Sources/App/version.swift
@@ -5,5 +5,5 @@
 //  Created by Robert Cheal on 5/9/24.
 //
 
-let myMusicServerVersion = "2.2.0" // You must manually sync this with the git tag
+let myMusicServerVersion = "2.2.1" // You must manually sync this with the git tag
 
